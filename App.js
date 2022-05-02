543d46e590a4cbef7e5ba6649a4d0545bbd800f5
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-05-02 10:52:50 +0200

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import Logo from "./assets/logo.png"
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image style={styles.tinyLogo} source={Logo} />
+            <Text style={styles.title}>AMAIZON</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    tinyLogo: {
+        width: 100,
+        height: 100,
+    },
+    title: {
+      fontWeight: "bold",
+      fontSize: 25
+    }
 });
