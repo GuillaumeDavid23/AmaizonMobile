5102b60bcd4d20c5d16bcd35d95dcdc84435067a
Benjamin Rault <b_rault@outlook.fr>
2022-05-05 13:03:04 +0200

    Create Login StackNavigator

---
+++ navigation/stackNavigation/LoginStackNav/LoginStackNav.js
@@ -0,0 +1,28 @@
+// React imports
+import React from "react";
+
+// Navigation imports
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Screen imports
+import LoginScreen from "../../../screens/LoginScreen";
+import TabNavigation from "../../tabNavigation/TabNavigation";
+
+// Creating Stack Navigator
+const Stack = createStackNavigator();
+
+const LoginStackNav = () => {
+    return (
+        <Stack.Navigator
+            // Remove headers from stacks
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="TabNavHome" component={TabNavigation} />
+        </Stack.Navigator>
+    );
+};
+
+export default LoginStackNav;
