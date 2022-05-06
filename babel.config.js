5f617d013a2ea9b7525c3caee5b34d67899f4aee
Benjamin Rault <b_rault@outlook.fr>
2022-05-06 15:32:09 +0200

    Prettier babel config

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ["babel-preset-expo"],
-        plugins: [
-            "react-native-paper/babel",
-            [
-                "module:react-native-dotenv",
-                {
-                    moduleName: "@env",
-                    path: ".env",
-                },
-            ],
-        ],
-    };
-};
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			'react-native-paper/babel',
+			[
+				'module:react-native-dotenv',
+				{
+					moduleName: '@env',
+					path: '.env',
+				},
+			],
+		],
+	}
+}
