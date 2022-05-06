ee93e708538bf7e330c2c660f7ed3cd005887754
Benjamin Rault <b_rault@outlook.fr>
2022-05-06 11:40:14 +0200

    Remove unused import

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 // React imports
 import * as React from 'react'
-import { KeyboardAvoidingView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 
 // Theme imports
@@ -9,7 +8,6 @@ import { Provider as ThemeProvider } from 'react-native-paper'
 
 // Route imports
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 import 'react-native-gesture-handler'
 
 // Redux imports
@@ -19,15 +17,12 @@ import { Provider as ReduxProvider } from 'react-redux'
 // Stack Screen imports
 import { LoginStackNav } from './navigation/stackNavigation'
 
-// Creating Stack Navigator
-const Stack = createStackNavigator()
-
 export default function App() {
 	return (
 		<ReduxProvider store={store}>
 			<ThemeProvider theme={theme}>
 				<StatusBar style="auto" hidden />
-				<NavigationContainer theme={theme}>
+				<NavigationContainer>
 					<LoginStackNav />
 				</NavigationContainer>
 			</ThemeProvider>
