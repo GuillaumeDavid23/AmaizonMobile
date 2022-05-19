922bfc63ab6dbf352ca7b19c6927ae1e64a9db54
Vincent Mancheron <vmancheron@yahoo.fr>
2022-05-19 15:48:18 +0200

    addProperty Mobile

--- navigation/stackNavigation/LoginStackNav/LoginStackNav.js
+++ navigation/stackNavigation/LoginStackNav/LoginStackNav.js
@@ -1,28 +1,30 @@
 // React imports
-import React from "react";
+import React from 'react'
 
 // Navigation imports
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack'
 
 // Screen imports
-import LoginScreen from "../../../screens/LoginScreen";
-import TabNavigation from "../../tabNavigation/TabNavigation";
+import LoginScreen from '../../../screens/LoginScreen'
+import TabNavigation from '../../tabNavigation/TabNavigation'
+import AddProperty from '../../../screens/AddProperty/AddProperty'
 
 // Creating Stack Navigator
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const LoginStackNav = () => {
-    return (
-        <Stack.Navigator
-            // Remove headers from stacks
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="TabNavHome" component={TabNavigation} />
-        </Stack.Navigator>
-    );
-};
+	return (
+		<Stack.Navigator
+			// Remove headers from stacks
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="Login" component={LoginScreen} />
+			<Stack.Screen name="TabNavHome" component={TabNavigation} />
+			<Stack.Screen name="AddProperty" component={AddProperty} />
+		</Stack.Navigator>
+	)
+}
 
-export default LoginStackNav;
+export default LoginStackNav
