a2a11a1105bee429209792055721c34e5e1b9363
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-06-02 11:00:41 +0200

    Custom

--- screens/ListInventory/components/CustomCard.js
+++ screens/ListInventory/components/CustomCard.js
@@ -5,6 +5,7 @@ import {
 	Dialog,
 	Portal,
 	Paragraph,
+	Title,
 } from 'react-native-paper'
 import { StyleSheet, View, Text, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -15,11 +16,21 @@ import { API_URL } from '@env'
 const CustomCard = (props) => {
 	const { inventory } = props
 	const [visible, setVisible] = React.useState(false)
-	console.log(inventory);
 	const showDialog = () => setVisible(true)
 
 	const hideDialog = () => setVisible(false)
-
+	const StatsName = {
+		electric: 'électrique',
+		gaz: 'gaz',
+		water: 'eau',
+	}
+	const EtatNames = {
+		1: 'Très mauvais',
+		2: 'Mauvais',
+		3: 'Moyen',
+		4: 'Bon',
+		5: 'Très bon',
+	}
 	return (
 		<Card style={styles.container}>
 			<Card.Title
@@ -45,7 +56,7 @@ const CustomCard = (props) => {
 				<Dialog
 					visible={visible}
 					onDismiss={hideDialog}
-					style={{ maxHeight: 400 }}
+					style={{ maxHeight: 600 }}
 				>
 					<Dialog.Title>
 						Etat des lieux du :{' '}
@@ -53,13 +64,47 @@ const CustomCard = (props) => {
 					</Dialog.Title>
 					<ScrollView>
 						<Dialog.Content>
-							{inventory.lst_statsMeters.map((stats, index) => {
-								return (<View key={index}>
-									<Text>{stats.name}</Text>
-									<Text>{stats.ref}</Text>
-									<Text>{stats.value}</Text>
-								</View>)
-							})}
+							<View>
+								<Title style={{ alignSelf: 'center' }}>
+									Relevé de compteur
+								</Title>
+								{inventory.lst_statsMeters.map(
+									(stats, index) => {
+										return (
+											<View key={index}>
+												<Title>
+													Compteur{' '}
+													{StatsName[stats.name]}
+												</Title>
+												<Text>
+													Référence : {stats.ref}
+												</Text>
+												<Text>
+													Montant : {stats.value}
+												</Text>
+											</View>
+										)
+									}
+								)}
+							</View>
+							<View>
+								<Title style={{ alignSelf: 'center' }}>
+									Etat des pièces :
+								</Title>
+								{inventory.lst_roomDetails.map(
+									(room, index) => {
+										return (
+											<View key={index}>
+												<Title>{room.name}</Title>
+												<Text>
+													Condition :{' '}
+													{EtatNames[room.condition]}
+												</Text>
+											</View>
+										)
+									}
+								)}
+							</View>
 						</Dialog.Content>
 					</ScrollView>
 					<Dialog.Actions>
